4b5cdc3d4f824f89d28bfdd94dcd674e83b7cc8f
alexYamaoka <ryamaoka1@gmail.com>
2020-09-01 19:30:20 -0700

    added print statements for dependency injection using setters

--- src/main/java/com/example/dependencyInjection/Student.java
+++ src/main/java/com/example/dependencyInjection/Student.java
@@ -2,16 +2,27 @@ package com.example.dependencyInjection;
 
 public class Student
 {
+    private int id;
     private String studentName;
 
 
+    public void setId(int id)
+    {
+        System.out.println("Inside setId method");
+        this.id = id;
+    }
+
     public void setStudentName(String studentName)
     {
+        System.out.println("Inside setStudentName method");
         this.studentName = studentName;
     }
 
     public void displayStudentInfo()
     {
+        System.out.println();
+        System.out.println("Displaying student info: ");
+        System.out.println("Student id is: " + id);
         System.out.println("Student name is: " + studentName);
     }
 }
