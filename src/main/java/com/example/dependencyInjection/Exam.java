bd98c6efc8dfe82baae89b8f07fb0f900cfffeac
alexYamaoka <ryamaoka1@gmail.com>
2020-09-01 19:45:30 -0700

    added constructor injection

--- src/main/java/com/example/dependencyInjection/Exam.java
+++ src/main/java/com/example/dependencyInjection/Exam.java
@@ -8,8 +8,17 @@ public class Exam
     public static void main(String[] args)
     {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-        Student student = applicationContext.getBean("student", Student.class);
 
+
+        Student student = applicationContext.getBean("alex", Student.class);
+        student.displayStudentInfo();
+
+
+        student = applicationContext.getBean("kevin", Student.class);
+        student.displayStudentInfo();
+
+
+        student = applicationContext.getBean("student", Student.class);
         student.displayStudentInfo();
     }
 }
--- src/main/java/com/example/dependencyInjection/Student.java
+++ src/main/java/com/example/dependencyInjection/Student.java
@@ -5,16 +5,27 @@ public class Student
     private int id;
     private String studentName;
 
+    public Student()
+    {
+        System.out.println("student default constructor");
+    }
+
+    public Student(int id, String studentName)
+    {
+        System.out.println("student constructor called");
+        this.id = id;
+        this.studentName = studentName;
+    }
 
     public void setId(int id)
     {
-        System.out.println("Inside setId method");
+        System.out.println("setId method called");
         this.id = id;
     }
 
     public void setStudentName(String studentName)
     {
-        System.out.println("Inside setStudentName method");
+        System.out.println("setStudentName called");
         this.studentName = studentName;
     }
 
