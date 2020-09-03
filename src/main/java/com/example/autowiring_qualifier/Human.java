3807c1944b883996b6fcec03cc5840fb65df9e29
alexYamaoka <ryamaoka1@gmail.com>
2020-09-02 21:00:10 -0700

    included autowire by constructor

--- src/main/java/com/example/autowiring_qualifier/Human.java
+++ src/main/java/com/example/autowiring_qualifier/Human.java
@@ -4,6 +4,14 @@ public class Human
 {
     private Heart heart;
 
+
+    public Human(Heart heart)
+    {
+        this.heart = heart;
+        System.out.println("Human constructor called");
+    }
+
+
     public void setHeart(Heart heart)
     {
         this.heart = heart;
