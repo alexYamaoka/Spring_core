4e4d0d3724f1c8bd7ac259710ad2961cd4efdc5a
alexYamaoka <ryamaoka1@gmail.com>
2020-09-02 18:36:56 -0700

    created new package for autowiring and qualifier

---
+++ src/main/java/com/example/autowiring_qualifier/Heart.java
@@ -0,0 +1,10 @@
+package com.example.autowiring_qualifier;
+
+public class Heart
+{
+    public void pump()
+    {
+        System.out.println("your heart is pumping");
+        System.out.println("alive");
+    }
+}
---
+++ src/main/java/com/example/autowiring_qualifier/Human.java
@@ -0,0 +1,6 @@
+package com.example.autowiring_qualifier;
+
+public class Human
+{
+    private Heart heart;
+}
