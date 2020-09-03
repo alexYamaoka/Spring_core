6ea4b906773d26824ff967fd9b752a709911645b
alexYamaoka <ryamaoka1@gmail.com>
2020-09-02 17:53:41 -0700

    added methods to class

--- src/main/java/com/example/loose_coupling/MathCheat.java
+++ src/main/java/com/example/loose_coupling/MathCheat.java
@@ -2,4 +2,8 @@ package com.example.loose_coupling;
 
 public class MathCheat
 {
+    public void mathCheat()
+    {
+        System.out.println("math cheating started...");
+    }
 }
--- src/main/java/com/example/loose_coupling/Student.java
+++ src/main/java/com/example/loose_coupling/Student.java
@@ -2,4 +2,15 @@ package com.example.loose_coupling;
 
 public class Student
 {
+    private MathCheat mathCheat;
+
+    public void setMathCheat(MathCheat mathCheat)
+    {
+        this.mathCheat = mathCheat;
+    }
+
+    public void cheating()
+    {
+        mathCheat.mathCheat();
+    }
 }
