0e6768843e7f39099f5e5756f73d3937ae97b4bf
alexYamaoka <ryamaoka1@gmail.com>
2020-09-02 18:26:38 -0700

    added javaCheat class

---
+++ src/main/java/com/example/loose_coupling/JavaCheat.java
@@ -0,0 +1,10 @@
+package com.example.loose_coupling;
+
+public class JavaCheat implements Cheat
+{
+    @Override
+    public void cheat()
+    {
+        System.out.println("java cheating started...");
+    }
+}
